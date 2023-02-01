4f6ae58e4619edd3e82b03258584f98abee1a483
Gustavo <avoguga@gmail.com>
2023-02-01 03:15:10 -0300

    Adding all

---
+++ twitterScript.js
@@ -0,0 +1,32 @@
+// Start script
+let twitterScriptObserver = null;
+
+const twitterScript = () => {
+  let body = document.body;
+  let views = document.querySelectorAll('[aria-label*="Exibir estatísticas do Tweet"]');
+  views.forEach((view) => {
+    view.parentElement.remove();
+  });
+
+  twitterScriptObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      let addedNodes = mutation.addedNodes;
+
+      for (let i = 0; i < addedNodes.length; i++) {
+        let node = addedNodes[i];
+        let views = node.querySelectorAll('[aria-label*="Exibir estatísticas do Tweet"]');
+        views.forEach((view) => view.parentElement.remove());
+      }
+    });
+  });
+
+  twitterScriptObserver.observe(body, { childList: true, subtree: true });
+};
+
+// Stop script
+const stopTwitterScript = () => {
+  if (twitterScriptObserver) {
+    twitterScriptObserver.disconnect();
+    twitterScriptObserver = null;
+  }
+};
